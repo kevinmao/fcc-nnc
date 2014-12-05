b7e4bd5e3cdf462c99a944fa115af9826319bc47
maojun <maojun@yahoo-inc.com>
2014-12-05 19:06:21 +0000

    calculate perplexity vs num-topics

---
+++ viz/perplexity.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# global vars
+source ../config.sh
+
+#######################################
+# calculate perplexity scores per num-topics
+#######################################
+for fv in `echo ${Fv_List}`; do
+    printmsg START $fv
+    pplx="${Mallet_Data}/${fv}/perplexity.txt"
+    rm -f ${pplx}; touch ${pplx}
+    for numTopics in {10..50..10}; do
+        python perplexity.py \
+            -p ${Mallet_Data}/${fv}/${numTopics}/evaluate.doc_probs.txt \
+            -l ${Mallet_Data}/${fv}/test.mallet.doc_lengths.txt \
+            -n ${numTopics} >> ${pplx}
+    done
+done
+
