f7574e371b2a4c1cf60742c9a7aeb5d0318f5cbb
maojun <maojun@yahoo-inc.com>
2014-12-06 18:23:41 +0000

    ngram plot log-perlexity comparison

--- viz/plot_pplx_ngram.m
+++ viz/plot_pplx_ngram.m
@@ -1,20 +1,22 @@
 clear; close all; clc;
 
 % load data
-DATA = dlmread ('../report/perplexity.txt', '\t', 1, 0);
+DATA = dlmread ('perplexity.txt', '\t', 1, 0);
 numTopic = DATA(:,1);
 unigram = log(DATA(:,2));
 bigram = log(DATA(:,3));
+unibigram = log(DATA(:,4));
 
 % plot
 figure; hold on; 
 plot(numTopic, unigram, 'bx-');
 plot(numTopic, bigram, 'rx-');
+plot(numTopic, unibigram, 'gx-');
 grid on;
 xlabel('Number of Topics');
 ylabel('Log-Perplexity');
-title('FCC Net Neutrality - LDA Topic Model');
-legend('unigram','bigram');
+title('FCC Net Neutrality - LDA Topic Modeling');
+legend('unigram','bigram', 'uni+bigram');
 
 % print
 print -dpng 'lda.perplexity.ngram.png';
